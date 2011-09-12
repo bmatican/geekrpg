9ff480cc69ffe61f448f5a7eca8bc64507a7f5dc
smirea <steven.mirea@gmail.com>
2011-09-13 00:09:00 +0200

    Filestructure first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title> GeekRPG! </title>
+      <link rel="stylesheet" type="text/css" href="css/main.css" />
+      <script type="text/javascript" src="js/jquery.js"></script>
+   </head>
+  
+   <body>
+      
+      <header>
+         <h1>Header</h1>
+         <nav>
+            <ul>
+               <li>Item</li>
+               <li>Item</li>
+               <li>Item</li>
+               <li>Item</li>
+            </ul>
+         </nav>
+      </header>
+      
+   </body>
+</html>
