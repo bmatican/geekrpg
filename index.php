454372c9540ba2bce724a9ac756a78c0de84144b
smirea <steven.mirea@gmail.com>
2011-09-13 02:12:52 +0200

    updated layout

--- index.php
+++ index.php
@@ -8,17 +8,45 @@
   
    <body>
       
-      <header>
-         <h1>Header</h1>
-         <nav>
+      <header id="header">
+         <h1>GeekRPG - ALPHA</h1>
+         <form action="login.php" method="post" id="login">
+            <input type="text" name="username" size="10" />
+            <input type="password" name="password" size="10" />
+            <input type="submit" value="Log In" />
+         </form>
+         <nav id="menu">
             <ul>
-               <li>Item</li>
-               <li>Item</li>
-               <li>Item</li>
-               <li>Item</li>
+               <li><a href="#">Item</a></li>
+               <li><a href="#">Item</a></li>
+               <li><a href="#">Item</a></li>
+               <li><a href="#">Item</a></li>
             </ul>
          </nav>
+         <div class="clearBoth"></div>
       </header>
+      
+      <aside id="featured" class="info">
+         <article>
+	         <figure>
+		         <img src="images/temp/sm-logo.gif" alt="Smashing Magazine" />
+	         </figure>
+	         
+	         <hgroup>
+		         <h2>Featured Article</h2>
+		         <h3><a href="#">HTML5 in Smashing Magazine!</a></h3>
+	         </hgroup>
+	         <p>Discover how to use Graceful Degradation and Progressive Enhancement techniques to achieve an outstanding, cross-browser <a href="http://dev.w3.org/html5/spec/Overview.html" rel="external">HTML5</a> and <a href="http://www.w3.org/TR/css3-roadmap/" rel="external">CSS3</a> website today!</p>
+         </article>
+      </aside>
+      
+      <section id="content">
+         
+      </section>
+      
+      <footer id="footer">
+         (C) CS Club 2011
+      </footer>
       
    </body>
 </html>
