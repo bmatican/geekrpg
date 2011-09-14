6b9b9925c82327d092c1bdeeb819d7c68c602af4
smirea <steven.mirea@gmail.com>
2011-09-14 16:21:35 +0200

    ...

--- src/registration/login.php
+++ src/registration/login.php
@@ -1,5 +1,11 @@
 <?php
 	require_once "db.php";
 
+   session_start();
 
+   function logIn( $username, $password ){
+      $query = mysql_query( "SELECT * FROM Users WHERE username='$username' AND password='$password'" );
+   }
+   
+   logIn( $_REQUEST['username'], $_REQUEST['password'] );
 ?>
