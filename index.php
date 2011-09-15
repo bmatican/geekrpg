0fadad7e113a569d42b22dde6b8b00a660ea6a92
smirea <steven.mirea@gmail.com>
2011-09-15 17:57:12 +0200

    url rewrite and views system

--- index.php
+++ index.php
@@ -1,45 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <title>RPGeek - ALPHA</title>
-      <link rel="stylesheet" type="text/css" href="/geekrpg/css/main.css" />
-      <script type="text/javascript" src="js/jquery.js"></script>
-      <script type="text/javascript" src="js/jquery.cookie.js"></script>
-      <link rel="stylesheet" type="text/css" href="css/jTooltip.css" />
-      <script type="text/javascript" src="js/jquery.jTooltip.js"></script>
-      <script type="text/javascript" src="js/default.js"></script>
-   </head>
-  
-   <body>
+  <head>
+    <title>RPGeek - ALPHA</title>
+    <link rel="stylesheet" type="text/css" href="/geekrpg/css/main.css" />
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.cookie.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jTooltip.css" />
+    <script type="text/javascript" src="js/jquery.jTooltip.js"></script>
+    <script type="text/javascript" src="js/default.js"></script>
+  </head>
+
+  <body>
+
+    <header id="header">
+      <h1>RPGeek - <span style="color:lightgreen">ALPHA</span></h1>
+      <form action="login.php" method="post" id="login">
+        <div>
+          <input type="text" name="username" size="10" placeholder="username" title="Username" />
+          <input type="password" name="password" size="10" placeholder="password" title="Password" />
+          <input type="submit" value="Log In" title="GO!" />
+        </div>
+      </form>
+      <nav id="menu">
+        <ul>
+          <li><a href="#">Menu Item</a></li>
+          <li><a href="#">Another Menu Item</a></li>
+          <li><a href="#">Yet Another Menu Item</a></li>
+          <li><a href="#">Item</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  <section id="content">
+    <?php
+      $q    = isset($_GET['q']) ? $_GET['q'] : 'home.php';
+      $file = "views/$q";
       
-      <header id="header">
-         <h1>RPGeek - <span style="color:lightgreen">ALPHA</span></h1>
-         <form action="login.php" method="post" id="login">
-            <div>
-               <input type="text" name="username" size="10" placeholder="username" title="Username" />
-               <input type="password" name="password" size="10" placeholder="password" title="Password" />
-               <input type="submit" value="Log In" title="GO!" />
-            </div>
-         </form>
-         <nav id="menu">
-            <ul>
-               <li><a href="#">Menu Item</a></li>
-               <li><a href="#">Another Menu Item</a></li>
-               <li><a href="#">Yet Another Menu Item</a></li>
-               <li><a href="#">Item</a></li>
-            </ul>
-         </nav>
-      </header>
-      
-      <section id="content">
-        <pre>
-          <?php var_dump( $_SERVER ); ?>
-        </pre>
-      </section>
-      
-      <footer id="footer">
-         (C) CS Club 2011
-      </footer>
-      
-   </body>
+      if( file_exists( $file ) ){
+        include_once( $file );
+      } else {
+        include_once( 'views/404.php' );
+      }
+    ?>
+  </section>
+
+  <footer id="footer">
+  (C) CS Club 2011
+  </footer>
+
+  </body>
 </html>
