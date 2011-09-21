7afdb11e1e5e4a38cbd2ec9225abebdf704a61f8
smirea <steven.mirea@gmail.com>
2011-09-21 04:32:54 +0200

    deleted minor file

--- scripts/sql/init_common.sql
+++
@@ -1,9 +0,0 @@
-/* user for doing user account specific operations (sign up, login, etc.) */
-
-CREATE USER 'noob'@'localhost' IDENTIFIED BY 'noob';
-CREATE DATABASE IF NOT EXISTS geekrpg;
-GRANT INSERT ON geekrpg.* TO 'noob'@'localhost';
-
-
-/* setup user table */
-CREATE TABLE IF NOT EXISTS geekrpg.Users (id INTEGER AUTO_INCREMENT PRIMARY KEY, username CHAR(20), password CHAR(32), email CHAR(30));
