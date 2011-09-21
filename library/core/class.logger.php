48fdc7e482a394a4359fc9b60073faaf3b9be232
Bogdan Matican <bogdanmatican@gmail.com>
2011-09-21 16:40:08 +0200

    Finished the singleton patter for Logger.

--- library/core/class.logger.php
+++ library/core/class.logger.php
@@ -2,6 +2,61 @@
 
 class Logger {
 
+  /**
+    * Container for singleton pattern.
+    */
+  private static $_instances = array();
+
+  /**
+    * Getter for singleton pattern.
+    */
+  public static function getInstance($_logLevel = self::ALL, $_logDirectory = FALSE) {
+    if (self::ALL < $_logLevel || self::NONE > $_logLevel) {
+      return NULL;
+    }
+
+    if (!isset($_instances[$_logLevel])) {
+      $_instances[$_logLevel] = new self($_logLevel, $_logDirectory);
+    }
+
+    return $_instances[$_logLevel];
+  }
+
+  /**
+    * Default constructor.
+    * @param $_level the default level at and above which to log
+    * @param $_logDirectory the default folder where to put the logs
+    * @throws Exception on OS write / open failures
+    */
+  private function __construct($_level = self::ALL, $_logDirectory = FALSE) {
+    $this->_level = $_level;
+    // default location
+    if (FALSE === $_logDirectory) {
+      $_logDirectory = "/tmp/geekrpglog";
+    }
+    $_logFilePath = $_logDirectory 
+      . DIRECTORY_SEPARATOR 
+      . "log_" 
+      . date("Y-m-d") 
+      . ".log";
+    $this->_logFilePath = $_logFilePath;
+    // extract directory
+    // $logDirectory = dirname($_logFilePath);
+    // create if not there
+    if (!file_exists($_logDirectory)) {
+      if(!mkdir($_logDirectory, 0777, true)) {
+        throw new Exception($this->_errorMessages['write']); 
+      }
+    }
+
+    // try to open it
+    $this->_fileHandler = fopen($this->_logFilePath, 'a');
+    if (FALSE === $this->_fileHandler) {
+      throw new Exception($this->_errorMessages['open']);
+    }
+  }
+
+
   // LOG levels
   const ALL = 0;
   const DEBUG = 1;
@@ -22,7 +77,7 @@ class Logger {
 
   private $_logFilePath;
 
-  public function setLevel($_level) {
+  public static function setLevel($_level) {
     if (Logger::ALL > $_level || Logger::NONE < $_level) {
       //TODO: retard :))
     } else {
@@ -30,11 +85,11 @@ class Logger {
     }
   }
 
-  public function getLevel() {
+  public static function getLevel() {
     return self::$_level;
   }
 
-  public function log($_level, $_message) {
+  public static function log($_level, $_message) {
     if ($this->_level <= $_level && self::NONE >= $_level) {
       $line = "";
       $line .= date("Y-m-d G:i:s");
@@ -50,7 +105,7 @@ class Logger {
     }
   }
 
-  private function _decodeLevel($_level) {
+  private static function _decodeLevel($_level) {
     switch ($_level) {
       case self::ALL :
         return "ALL";
@@ -68,45 +123,6 @@ class Logger {
         return "NONE";
       default :
         return "LOG";
-    }
-  }
-
-  /**
-    * Default constructor.
-    * @param $_level the default level at and above which to log
-    * @param $_logDirectory the default folder where to put the logs
-    * @throws Exception on OS write / open failures
-    */
-  public function __construct($_level = self::ALL, $_logDirectory = FALSE) {
-    $this->_level = $_level;
-    // default location
-    if (FALSE === $_logDirectory) {
-      $_logDirectory = "/tmp/geekrpglog";
-    }
-    $_logFilePath = $_logDirectory 
-      . DIRECTORY_SEPARATOR 
-      . "log_" 
-      . date("Y-m-d") 
-      . ".log";
-    $this->_logFilePath = $_logFilePath;
-    // extract directory
-    // $logDirectory = dirname($_logFilePath);
-    // create if not there
-    if (!file_exists($_logDirectory)) {
-      if(!mkdir($_logDirectory, 0777, true)) {
-        throw new Exception($this->_errorMessages['write']); 
-      }
-    }
-    // no write access?
-    // if (!is_writable($this->_logFilePath)) {
-    //   var_export(is_writable($this->_logFilePath));
-    //   throw new Exception($this->_errorMessages['write']);
-    // }
-
-    // try to open it
-    $this->_fileHandler = fopen($this->_logFilePath, 'a');
-    if (FALSE === $this->_fileHandler) {
-      throw new Exception($this->_errorMessages['open']);
     }
   }
 
