8e26e300a8eda6933f2bf962f752d0ef7411b06b
Stefan Mirea <steven.mirea@gmail.com>
2011-12-05 15:43:25 +0100

    tags update

--- applications/core/helpers/class.handlers.php
+++ applications/core/helpers/class.handlers.php
@@ -9,6 +9,7 @@ class CoreHandlers extends Geek_Handlers {
     $tagControllers = array("PostController", "ProblemController");
     foreach ($tagControllers as $t) {
       $this->registerMethod($t, "createTag");
+      $this->registerMethod($t, "editTag");
       $this->registerMethod($t, "destroyTag");
       $this->registerMethod($t, "viewTag");
       $this->registerMethod($t, "indexTags");
@@ -38,12 +39,44 @@ class CoreHandlers extends Geek_Handlers {
         $view->add( new Anchor('create another one', array('href' => Geek::path($controller->CONTROLLER_NAME.'/createTag') ) ) );
         $view->add( "<div>The tag <b>$name</b> with the description <b>$description</b> has been created!</div>" );
       } else {
-    $view->add( new Anchor('create another one', array('href' => Geek::path($controller->CONTROLLER_NAME.'/createTag') ) ) );
+        $view->add( new Anchor('create another one', array('href' => Geek::path($controller->CONTROLLER_NAME.'/createTag') ) ) );
         $view->add( "<div>An error has occured: <span style=\"color:red\">$result</span></div>" );
       }
       $controller->render( $view );
     } else {
       $form = $view->Form( 'createTag', $controller->CONTROLLER_NAME.'/createTag', 'name,description' );
+      $form->input( 'name', array( 'placeholder' => 'name' ) );
+      $form->input( 'description', array( 'placeholder' => 'description', 'size' => 40 ) );
+      $form->submit( 'Create Tag!' );
+      $controller->render( $view );
+    }
+  }
+  
+  public function editTag($controller, $name = null, $description = null) {
+    $info = $controller->tagModel->getAllWhere( array('name="'.$name.'"') );
+    if( empty($info) ){
+      $controller->renderError( '404' );
+      return;
+    }
+    $info = $info[0];
+    $view = Geek::getView( 'blank' );
+    if ($name && $description) {
+      $result = $controller->tagModel->update(array(
+        "id"          => $info['id'],
+        "name"        => $name,
+        "description" => $description
+      ));
+      if( true === $result ){
+        $view->add( new Anchor('back to tags', array('href' => Geek::path($controller->CONTROLLER_NAME.'/indexTags') ) ) );
+        $view->add( "<div>The tag with the new <b>$name</b> with the new description <b>$description</b> has been edited!</div>" );
+      } else {
+        $view->add( new Anchor('back to tags', array('href' => Geek::path($controller->CONTROLLER_NAME.'/indexTags') ) ) );
+        $view->add( "<div>An error has occured: <span style=\"color:red\">$result</span></div>" );
+      }
+      $controller->render( $view );
+    } else {
+      $form = $view->Form( 'editTag', $controller->CONTROLLER_NAME.'/editTag', 'name,description' );
+      $form->setValues( $info );
       $form->input( 'name', array( 'placeholder' => 'name' ) );
       $form->input( 'description', array( 'placeholder' => 'description', 'size' => 40 ) );
       $form->submit( 'Create Tag!' );
@@ -73,10 +106,21 @@ class CoreHandlers extends Geek_Handlers {
   
   public function indexTags($controller, $limit = null, $offset = null, $orderby = 'name') {
     $tags = $controller->tagModel->getAllWhere(array("id > 0"), $limit, $offset, null, $orderby);
-    geek::export($tags);
     
-    //TODO: wish I had a view like yours...
-    $controller->render('index', $tags);
+    $view = Geek::getView('Blank');
+    
+    foreach( $tags as $k => $v ){
+      $href = Geek::path( $controller->CONTROLLER_NAME.'/tags/'.$v['name'] );
+      $edit = Geek::path( $controller->CONTROLLER_NAME.'/editTag/'.$v['name'] );
+      $view->add('<div class="post">
+          <span class="tag"><a href="'.$href.'">'.$v['name'].'</a></span>
+          - '.$v['description']
+         . '<div class="meta"><a href="'.$edit.'">edit</a></div>'
+        .'</div>'
+      );
+    }
+    
+    $controller->render( $view );
   }
 }
 
--- applications/core/views/post/view.index.php
+++ applications/core/views/post/view.index.php
@@ -20,7 +20,8 @@
       
       $div = new HtmlContainer(
               'div',
-              '<a href="'.Geek::path( $args['controller'].'/createTag' ).'"> Create Tag </a> |
+              '<a href="'.Geek::path( $args['controller'].'/indexTags' ).'"> View tags </a> |
+              <a href="'.Geek::path( $args['controller'].'/createTag' ).'"> Create Tag </a> |
               <a href="'.Geek::path( $args['controller'].'/add' ).'"> Post </a>',
               array( 'style' => "text-align:right;margin-bottom:5px;padding:2px;border-bottom:1px solid #ccc;" ),
               'p_loggedin'
