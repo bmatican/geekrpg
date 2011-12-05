473cb000afd419d4ed21d2adda0bd72d92706ef1
Stefan Mirea <steven.mirea@gmail.com>
2011-12-05 16:15:23 +0100

    added multi-level comments

--- library/core/views/view.comment.php
+++ library/core/views/view.comment.php
@@ -17,35 +17,51 @@ class Comment extends GeekView{
     
     $form = $this->Form( $args['form_name'], $args['form_action'], $args['form_fields'], $args['form_attributes'] );
     $form->getValues( $args );
+    $form->input('postid', array( 'type' => 'hidden', 'value' => $args['id'] ) );
+    $form->textarea('body', array( 'placeholder'=>'Spam your comment here please', 'title'=>'You should not spam though... comment wisely or funny'));
+    $form->input('parentid', array( 'type' => 'hidden', 'value' => 0 ) );
+    $form->input('submit', array('type'=>'submit', 'value'=>'Comment'));
 
+    $this->prepend( $this->_generateComments( $args, $args['comments'] ) );
+  }
+  
+  private function _generateComments( $args, $comments ){
     $h = '';
-    foreach( $args['comments'] as $v ){
+    
+    foreach( $comments as $v ){
       $c = $v['value'];
       //$user = 'UID('.$c['userid'].')';
       $user = $c['username'];
       $time = formatTime( timeVals( time() - intVal($c['dateAdded']) ) );
+      $comment = Geek::getView( 'Blank' );
+      $commentForm = $comment->Form( 'comment', $args['form_action'].'/'.$c['id'], $args['form_fields'], array('style'=>'display:none') );
+      $commentForm->input('postid', array( 'type' => 'hidden', 'value' => $args['id'] ) );
+      $commentForm->textarea('body', array( 'placeholder'=>'Spam your comment here please', 'title'=>'You should not spam though... comment wisely or funny'));
+      $commentForm->input('parentid', array( 'type' => 'hidden', 'value' => $c['id'] ) );
+      $commentForm->input('submit', array('type'=>'submit', 'value'=>'Comment'));
+      $commentForm = $commentForm->toString();
+      $children = '';
+      if( !empty( $v['children'] ) ){
+        $children .= $this->_generateComments( $args, $v['children'] );
+      }
       $h .= <<<COMMENT
         <div class="comment">
           <div class="body">$c[body]</div>
           <div class="actions">
             <span class="meta"> <i>$time</i> ago by <b>$user</b> </span>
             <div class="buttons">
-              <a href="javascript:void(0)">Reply</a>
+              <a href="javascript:void(0)" onclick="$(this).next().next().slideToggle()">Reply</a>
               <a href="javascript:void(0)">Delete</a>
+              $commentForm
+              $children
             </div>
           </div>
         </div>
 COMMENT;
     }
-
-    $form->add( $h );
-    $form->input('postid', array( 'type' => 'hidden', 'value' => $args['id'] ) );
-    $form->textarea('body', array( 'placeholder'=>'Spam your comment here please', 'title'=>'You should not spam though... comment wisely or funny'));
-    $form->input('parentid', array( 'type' => 'hidden', 'value' => 0 ) );
-    $form->input('submit', array('type'=>'submit', 'value'=>'Comment'));
-  
+    return $h;
   }
-
+  
 }
 
 ?>
